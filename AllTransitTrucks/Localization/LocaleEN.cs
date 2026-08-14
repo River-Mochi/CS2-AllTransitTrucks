ab0a6bb06a7a579f6c480f49732f477ce14ee9b9
River Mochi <97197236+River-Mochi@users.noreply.github.com>
2026-08-14 15:48:25 -0700

    update EN tidy

--- AllTransitTrucks/Localization/LocaleEN.cs
+++ AllTransitTrucks/Localization/LocaleEN.cs
@@ -221,8 +221,8 @@ namespace PublicWorksPlus
                 { m_Setting.GetOptionLabelLocaleID(nameof(ATTSettings.EnableCompanyTruckControl)), "Show Industry Trucks" },
                 { m_Setting.GetOptionDescLocaleID(nameof(ATTSettings.EnableCompanyTruckControl)),
                     "For Compatibility with other mods,\n" +
-                    "- use OFF if you prefer to use another mod to control the same industry/company total of trucks.\n" +
                     "<[x] Default ON>.\n" +
+                    "- use OFF if you prefer to use another mod to control the same industry/company total of trucks.\n" +
                     "Leave ON to use the three Industrial company-truck sliders for adjusting total trucks numbers.\n" +
                     "Turn OFF to restore these 3 sliders to game default and hide the sliders.\n" +
                     "If you prefer to use this mod's sliders, then check if the other mod has a toggle to disable its own truck numbers."
